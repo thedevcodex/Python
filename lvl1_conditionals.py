578cfebf9195434cfbf1b693070a1d13ac22f00e
TheDevJournal <s.sudha19082006@gmail.com>
2026-03-04 19:04:16 +0530

    Create lvl1_conditionals.py

---
+++ lvl1_conditionals.py
@@ -0,0 +1,71 @@
+#Check if a number is positive.
+num=5
+if num>0:
+    print(f"{num} is Positive")
+else:
+    print(f"{num} is Not postive")
+
+print("-------------------------------------------------------")
+#Check if a number is negative.
+num1=-9
+if num1<0:
+    print(f"{num1} is Negative")
+else:
+    print(f"{num1} is Not Negative")
+print("-------------------------------------------------------")
+#Check if a number is zero.
+num2=0
+if num2==0:
+    print(f'{num2} is Zero')
+else:
+    print(f"{num2} is Not Zero")
+print("-------------------------------------------------------")
+#Check if a number is even or odd.
+num3=10
+if num3 % 2==0:
+    print(f"{num3} is Even")
+else:
+    print(f'{num3} is Odd')
+print("-------------------------------------------------------")
+#Check if a number is divisible by 5.
+num4=20
+if num4%5==0:
+    print(f"{num4} is divisble by 5")
+else:
+    print(f"{num4} is not divisible by 5")
+print("-------------------------------------------------------")
+#Compare two numbers and print the greater one.
+num_1=30
+num_2=15
+if num_1>num_2:
+    print(f"{num_1} is greater")
+else:
+    print(f"{num_2} is greater")
+print("-------------------------------------------------------")
+#Check if a person is eligible to vote (age ≥ 18).
+age=19
+if age>=18:
+    print(f"{age} is Eligible to vote")
+else:
+    print(f'{age} is Not Eligible to vote')
+print("-------------------------------------------------------")
+#Check if a number is greater than 100.
+number=300
+if num >100:
+    print(f"{number} is Greater than 100")
+else:
+    print(f"{number} is Not Greater than 100")
+print("-------------------------------------------------------")
+#Check whether a character is uppercase or lowercase.
+character="I"
+if character==(character.upper()):
+    print(f"{character} is Uppercase")
+else:
+    print(f"{character} is Lowercase")
+print("-------------------------------------------------------")
+#Check if a year is divisible by 4.
+year=2021
+if year%4==0:
+    print(f'{year} is divisible by 4')
+else:
+    print(f"{year} is not divisible by 4")
